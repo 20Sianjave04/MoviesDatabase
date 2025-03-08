d7f8d421808840ce361575fbebb8b8949dfe79e1
Jacob Ahrens <jahrens1003@gmail.com>
2025-03-07 19:05:27 -0500

    Update MOVIES_queries.sql

--- SQlite/MOVIES_queries.sql
+++ SQlite/MOVIES_queries.sql
@@ -1,26 +1,27 @@
+.www
 SELECT title, year, length 
 FROM Movie
 WHERE studioID = (SELECT studioID FROM Studio WHERE name = 'MGM')
 AND year > 1990 
 AND length > 90;
-.www
 
+.www
 SELECT title, length 
 FROM Movie
 ORDER BY length ASC;
-.www
 
+.www
 SELECT name 
 FROM Actor
 WHERE gender = 'Female'
 ORDER BY name ASC;
-.www
 
+.www
 SELECT name 
 FROM Studio
 WHERE studioID IN (SELECT studioID FROM Movie WHERE title LIKE 'Star Wars%');
-.www
 
+.www
 SELECT title, year, 
     (SELECT name FROM Studio WHERE Studio.studioID = Movie.studioID) AS Studio, 
     rating 
@@ -28,20 +29,20 @@ FROM Movie
 WHERE movieID IN (SELECT movieID FROM StarsIn WHERE personID = 
     (SELECT personID FROM Actor WHERE name = 'Leonardo DiCaprio'))
 ORDER BY year ASC;
-.www
 
+.www
 SELECT name, 
     (SELECT SUM(length) FROM Movie WHERE Movie.studioID = Studio.studioID) AS total_length 
 FROM Studio;
-.www
 
+.www
 SELECT name 
 FROM Studio
 WHERE city = 'Los Angeles';
-.www
 
+.www
 SELECT name, 
     (SELECT AVG(rating) FROM Movie WHERE movieID IN 
         (SELECT movieID FROM StarsIn WHERE StarsIn.personID = Actor.personID)) AS avg_rating 
 FROM Actor;
-.www
+
